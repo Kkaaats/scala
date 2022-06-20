9fd59586f62fc60076419edc815d997c64aab8d2
Kkaaats <ekaterinaharris03@gmail.com>
2022-06-21 00:08:11 +0300

    'init'

--- src/main/scala/Optional.scala
+++ src/main/scala/Optional.scala
@@ -1,69 +1,143 @@
-package com.tkroman.kpi.y2022.l1
+package BoolExpr
+//import scala.annotation.varargs
+//import com.tkroman.kpi.y2022.l1._
 
-enum Optional[+A]:
-  case None
-  case Some(x: A)
-
-def fold[A, B](a: Optional[A], z: B, f: (B, A) => B) =
-  a match {
-    case Optional.None => z
-    case Optional.Some(x) => f(z, x)
-  }
-
-enum Tree[+A]:
-  case Branch(l: Tree[A], r: Tree[A])
-  case Leaf(a: A)
-
-enum Set[+A]:
-  case Empty
-  case NonEmpty private (a: A, rest: Set[A])
-object Set:
-  def makeSet[A](xs: A*): Set[A] = ???
-
-case class Bag[A] private (private val map: Map[A, Int])
-object Bag:
-  def makeBag[A](xs: A*): Bag[A] = ???
-
-// https://en.wikipedia.org/wiki/Binary_search_tree
-case class BSTree private (l: Option[BSTree], r: Option[BSTree], v: Int)
-object BSTree:
-  def makeBst(xs: Int*): BSTree = ???
-
-enum List[+A]:
-  case Nil
-  case Cons(hd: A, tl: List[A])
-
-enum IntOpCode:
-  case Add, Mul
-
-enum BoolOpCode:
-  case And, Or
-
-enum IntExpr:
-  case Lit(n: Int)
-  case Op(opCode: IntOpCode, l: IntExpr, r: IntExpr)
+enum Op:
+  case And, Or, Neg
+  def toStr(op: Op = this): String =
+    op match
+      case And => "&&"
+      case Or  => "||"
+      case Neg => "!"
 
 enum BoolExpr:
+  case Nil
   case Lit(b: Boolean)
-  case Op(opCode: BoolOpCode, l: BoolExpr, r: BoolExpr)
+  case Variable(b: String)
+  case Expr(op: Op, l: BoolExpr, r: BoolExpr)
+  override def toString(): String =
+    def run(e: BoolExpr = this): String =
+      e match
+        case Nil         => ""
+        case Variable(b) => "(" + b + ")"
+        case Lit(b)      => if b then "(true)" else "(false)"
+        case Expr(op, l, r) => {
+          if op == Op.Neg then { "(!" + l.toString() + ")" }
+          else { "(" + l.toString() + op.toStr() + r.toString() + ")" }
+        }
+    run()
+  def eval(e: BoolExpr = this): Boolean =
+    def run(expr: BoolExpr = e): Boolean =
+      expr match
+        case Lit(b)               => if b then true else false
+        case Expr(Op.And, l, r)   => if run(l) && run(r) then true else false
+        case Expr(Op.Or, l, r)    => if run(l) || run(r) then true else false
+        case Expr(Op.Neg, l, Nil) => if run(l) then false else true
+    run()
+  def getVariables(expr: BoolExpr = this): List[String] =
+    var vars = Set[String]()
+    def run(expression: BoolExpr = expr): Any =
+      expression match
+        case Variable(b) => vars = vars + b
+        case Expr(op, l, r) => {
+          run(l)
+          run(r)
+        }
+        case _ =>
+    run()
+    vars.toList
 
-enum Nat:
-  case Zero
-  case Succ(n: Nat)
+  def setVariable(
+                   variable: String,
+                   value: Boolean
+                 ): BoolExpr =
+    def run(expression: BoolExpr = this): BoolExpr =
+      expression match
+        case Expr(op, l, r) => Expr(op, run(l), run(r))
+        case Lit(b)         => Lit(b)
+        case Nil            => Nil
+        case Variable(b)    => if b == variable then Lit(value) else Variable(b)
+    run()
 
-case class Rational(num: Int, denom: Int)
+  def getValues(
+                 right: BoolExpr,
+                 left: BoolExpr = this
+               ): Set[(String, Boolean)] =
+    var values = Set[(String, Boolean)]()
+    def run(
+             expr: BoolExpr = left,
+             init: BoolExpr = right
+           ): Set[(String, Boolean)] =
+      (expr, init) match
+        case (Nil, Nil)       => values
+        case (Lit(a), Lit(b)) => values
+        case (Variable(a), Lit(b)) => {
+          values = values + ((a, b))
+          values
+        }
+        case (Expr(a, b, c), Expr(d, e, f)) => {
+          run(b, e)
+          run(c, f)
+        }
+    run()
 
-enum Compared:
-  case Lt, Gt, Eq // < > =
+  def varToBool(expr: BoolExpr = this): List[Set[(String, Boolean)]] =
+    var result = List[Set[(String, Boolean)]]()
+    var bools = Set[BoolExpr](expr)
+    def run(
+             expression: BoolExpr = expr,
+             variables: List[String] = expr.getVariables(),
+             cnt: Int = 0
+           ): Set[BoolExpr] =
+      var bool = Set[BoolExpr]()
+      if cnt < variables.length then {
+        bools.foreach(e => bool += e.setVariable(variables(cnt), true))
+        bools.foreach(e => bool += e.setVariable(variables(cnt), false))
+        bools = bool
+        run(expr, expr.getVariables(), cnt + 1)
+      } else bools
+    run()
+    bools.foreach(myexpr => {
+      if myexpr.eval() == true then result = result :+ expr.getValues(myexpr)
+    })
+    result
 
-enum RecEntry[A]:
-  case Flat(a: A)
-  case Nested(as: List[RecEntry[A]])
+object BoolExpr {
+  def apply(value: Boolean): BoolExpr =
+    Lit(value)
+  def apply(value: String): BoolExpr =
+    Variable(value)
+  def apply(op: Op, l: BoolExpr): BoolExpr =
+    Expr(op, l, Nil)
+  def apply(op: Op, l: String): BoolExpr =
+    Expr(op, BoolExpr(l), Nil)
+  def apply(op: Op, l: Boolean): BoolExpr =
+    Expr(op, BoolExpr(l), Nil)
+  def apply(op: Op, l: BoolExpr, r: BoolExpr): BoolExpr =
+    Expr(op, l, r)
+  def apply(op: Op, l: Boolean, r: BoolExpr): BoolExpr =
+    Expr(op, BoolExpr(l), r)
+  def apply(op: Op, l: BoolExpr, r: Boolean): BoolExpr =
+    Expr(op, l, BoolExpr(r))
+  def apply(op: Op, l: Boolean, r: Boolean): BoolExpr =
+    Expr(op, BoolExpr(l), BoolExpr(r))
+  def apply(op: Op, l: String, r: BoolExpr): BoolExpr =
+    Expr(op, BoolExpr(l), r)
+  def apply(op: Op, l: BoolExpr, r: String): BoolExpr =
+    Expr(op, l, BoolExpr(r))
+  def apply(op: Op, l: String, r: String): BoolExpr =
+    Expr(op, BoolExpr(l), BoolExpr(r))
+  def apply(op: Op, l: String, r: Boolean): BoolExpr =
+    Expr(op, BoolExpr(l), BoolExpr(r))
+  def apply(op: Op, l: Boolean, r: String): BoolExpr =
+    Expr(op, BoolExpr(l), BoolExpr(r))
+}
 
-// NOTE: do not use this to demo the results.
-// Use unit-tests instead
 @main def run() =
-  println("Hello")
+  var first = BoolExpr(Op.And, BoolExpr(Op.Or, true, "x"),"y")
+  println(first.varToBool())
+
+
 
 
 
--- src/test/scala/OptionalSuite.scala
+++ src/test/scala/OptionalSuite.scala
@@ -1,16 +1,41 @@
-package com.tkroman.kpi.y2022.l1
-
+package Lst
 import munit.FunSuite
+import BoolExpr.*
+import BoolExpr._
 
-class OptionalSuite extends FunSuite {
-  test("fold on Some") {
-    val expected = 10
-    val actual = fold(Optional.Some(5), 0, (_, a) => a * 2)
+class LstTest extends FunSuite {
+  test("eval") {
+    val expected = true
+    val actual = BoolExpr(Op.And, true, BoolExpr(Op.Or, true, false)).eval()
     assertEquals(actual, expected)
   }
-  test("fold on None") {
-    val expected = 0
-    val actual = fold(Optional.None: Optional[Int], 0, (_, a) => a * 2)
+  test("eval") {
+    val expected = false
+    val actual = BoolExpr(Op.And, false, BoolExpr(Op.Or, true, false)).eval()
+    assertEquals(actual, expected)
+  }
+  test("getVariables") {
+    val expected = List("x", "y")
+    val actual =
+      BoolExpr(Op.And, "x", BoolExpr(Op.Or, "y", false)).getVariables()
+    assertEquals(actual, expected)
+  }
+  test("setVariable") {
+    val expected = BoolExpr(Op.And, true, BoolExpr(Op.Or, "y", false))
+    val actual =
+      BoolExpr(Op.And, "x", BoolExpr(Op.Or, "y", false)).setVariable("x", true)
+    assertEquals(actual, expected)
+  }
+  test("getValues") {
+    val not_assigned = BoolExpr(Op.And, "x", BoolExpr(Op.Or, "y", false))
+    val assigned = BoolExpr(Op.And, true, BoolExpr(Op.Or, false, false))
+    val expected = Set(("x", true), ("y", false))
+    val actual = not_assigned.getValues(assigned)
+    assertEquals(actual, expected)
+  }
+  test("varToBool") {
+    val expected = Set(Set(("x", true), ("y", true)))
+    val actual = BoolExpr(Op.And, "x", BoolExpr(Op.Or, "y", false)).varToBool()
     assertEquals(actual, expected)
   }
 }
